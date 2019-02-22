235210ab0c930e20fe7c51e7ea229c0c36f59f76
Duk Lee <dukhee@gmail.com>
2019-02-22 06:09:30 -0800

    python solution for 1st one

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -1,9 +1,14 @@
 # Uses python3
 import sys
 
+
 def get_change(m):
-    #write your code here
-    return m
+    # write your code here
+    num_of_tens = m // 10
+    num_of_fives = (m % 10) // 5
+    num_of_ones = m % 5
+    return num_of_tens + num_of_fives + num_of_ones
+
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,9 +1,32 @@
 # Uses python3
 import sys
+from operator import itemgetter
+
 
 def get_optimal_value(capacity, weights, values):
     value = 0.
     # write your code here
+    # evaluate value per weight
+    # add highest value per weight item till either the bag is full or you use up all the item
+    # repeat the same thing to the next highest value per weight item.
+    # continue till the bag is full
+    value_per_weight = []
+    i = 0
+    for weight in weights:
+        value_per_weight.append({'total_weight': weight, 'value_per_weight': values[i]/weight})
+        i = i + 1
+
+    new_list = sorted(value_per_weight, key=itemgetter('value_per_weight'), reverse=True)
+    available_weight = capacity
+
+    for item in new_list:
+        if available_weight != 0 and item['total_weight'] <= available_weight:
+            # add everything to the sack
+            value = value + item['value_per_weight'] * item['total_weight']
+            available_weight = available_weight - item['total_weight']
+        elif available_weight != 0 and item['total_weight'] > available_weight:
+            value = value + item['value_per_weight'] * available_weight
+            available_weight = 0
 
     return value
 
